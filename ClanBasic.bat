0d46cad6217422234fd9f2610016fc4d47626e4d
TeamAntiLagDeveloper <jdcp711@gmail.com>
2024-10-08 20:25:35 -0300

    Remover arquivos indesejados

--- ClanBasic.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-color 0d
-del /q /s %TEMP%\*
-del /q /s %SystemRoot%\Temp\*
-del /q /s %SystemRoot%\Prefetch\*.*
-exit
--- CleanDefault.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-color 0d
-del /q /s /f "%TEMP%\*.*"
-del /q /s /f "%SystemRoot%\Temp\*.*"
-del /q /s /f "%SystemRoot%\Prefetch\*.*"
-del /q /s /f "%SystemRoot%\System32\winevt\Logs\*.*"
-del /q /s /f "%SystemDrive%\inetpub\logs\LogFiles\*.*"
-exit
